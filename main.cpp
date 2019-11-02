ae034d0efed6d59d339889e3c33d19bba060effa
Jordan Hsu <qaz7821819@gmail.com>
2019-11-02 10:59:28 +0800

    [Fix] some warning log

--- main.cpp
+++ main.cpp
@@ -48,25 +48,26 @@ vector<string> string_split(const string &s, const string &seperator){
         int flag = 0;
         while(i != s.size() && flag == 0){
             flag = 1;
-            for(string_size x = 0; x < seperator.size(); ++x)
-                　　if(s[i] == seperator[x]){
-                    　　++i;
-                    　　flag = 0;
-                    　　 break;
-                    　　}
+            for(string_size x = 0; x < seperator.size(); ++x){
+                if(s[i] == seperator[x]){
+                    ++i;
+                    flag = 0;
+                    break;
+                }
+            }
         }
          
         //找到又一個分隔符，將兩個分隔符之間的字串取出；
         flag = 0;
         string_size j = i;
         while(j != s.size() && flag == 0){
-            for(string_size x = 0; x < seperator.size(); ++x)
-                　　if(s[j] == seperator[x]){
-                    　　flag = 1;
-                    　　 break;
-                    　　}
-            if(flag == 0)
-                　　++j;
+            for(string_size x = 0; x < seperator.size(); ++x){
+                if(s[j] == seperator[x]){
+                    flag = 1;
+                    break;
+                }
+            }
+            if(flag == 0) ++j;
         }
         if(i != j){
             result.push_back(s.substr(i, j-i));
